b4421315a62e2f76ea5065ad1947e15a99bb7475
toyboxZ <6660197+toyboxZ@users.noreply.github.com>
2020-12-26 22:53:54 +0800

    fix add script for clone dp branch and neos

--- files/software/clone_dp.sh
+++ files/software/clone_dp.sh
@@ -3,6 +3,7 @@
 cat "$0"|grep -v grep | grep -lUP '\r$' >/dev/null && echo "先执行dos2unix '$0'" && exit
 
 branch="$1"
+updater_md5=6933f79b9e0e616a5bdedc7d8dd2dafe
 git_dp_uri="/dragonpilot-community/dragonpilot.git"
 git_host_list="hub.fastgit.org github.com.cnpmjs.org github.com"
 git_host=github.com
@@ -18,7 +19,7 @@ check_git_host() {
     for ip in $(curl -m 2 "http://119.29.29.29/d?dn=${host}.&ttl=1" 2>/dev/null|sed 's/;/ /g'|awk -F ',' '{print $1}');do
       echo $ip|grep -E '^[1-9]*[0-9]*\.[0-9]+\.[0-9]+\.[0-9]+$' >/dev/null
       if [ $? -eq 0 ];then
-        c=$(ping  -c 5 -i0.2 -W1 $ip 2>/dev/null |grep 'min/avg/max/mdev'|awk -F [=/] '{print $(NF-2)}'|awk -F '.' '{print $1}'); [ "$c" = "" ] && c=10000
+        c=$(ping  -c 3 -i0.2 -W1 $ip 2>/dev/null |grep 'min/avg/max/mdev'|awk -F [=/] '{print $(NF-2)}'|awk -F '.' '{print $1}'); [ "$c" = "" ] && c=10000
         [ $c -lt $lc ] && lc=$c && lip=$ip
         [ $c -lt $gc ] && gc=$c && git_host=$host
       fi
@@ -34,6 +35,12 @@ check_git_host() {
     fi
   done
   
+}
+
+clean_hosts(){
+  for host in $git_host_list ;do
+    sed -r -i "/.*[\t ]*${host}\$/d" /etc/hosts
+  done
 }
 
 get_git_branchs() {
@@ -109,18 +116,19 @@ set_branch() {
 }
 
 update_neos() {
-  echo "[$(date +'%F %T')] 开始下载NEOS v.${REQUIRED_NEOS_VERSION}"
+  echo "[$(date +'%F %T')] 准备下载NEOS和recovery v.${REQUIRED_NEOS_VERSION}"
   [ -d "/data/neoupdate" ] || mkdir "/data/neoupdate"
   
   cd "/data/neoupdate"
-  file updater | grep 'ELF' >/dev/null || wget -T 60 "http://wiki.dragonpilot.cn/files/software/neosupdate/updater" -O updater
+  lmd5="$(md5sum updater|awk '{print $1}')"
+  [ "$updater_md5" != "$lmd5" ] && wget -T 60 "http://wiki.dragonpilot.cn/files/software/neosupdate/updater" -O updater
   file updater | grep 'ELF' >/dev/null
   if [ $? -ne 0  ];then
     echo "[$(date +'%F %T')] 获取NEOS下载器失败"
     exit 1
   fi
   chmod +x updater
-  killall -9 ai.comma.plus.offroad
+  killall -9 ai.comma.plus.offroad 2>/dev/null
   
   update_url="http://wiki.dragonpilot.cn/files/software/neosupdate/update.${REQUIRED_NEOS_VERSION}.json"
   #update_url="http://192.168.2.106/neosupdate/update.${REQUIRED_NEOS_VERSION}.json"
@@ -140,7 +148,7 @@ update_neos() {
       ./updater "$update_url" >> updater.log 2>&1 &
     fi
     sleep 1
-    [ -f /dev/progress.info] && read l < /dev/progress.info 2>/dev/null && echo -n "\r$l\t\t\t"
+    [ -f /dev/progress.info ] && read l < /dev/progress.info 2>/dev/null && echo -n "\r$l\t\t\t"
   done
   
 }
@@ -163,6 +171,8 @@ while [ $? -ne 0 ];do
   git_clone
 done
 s2=$(date +%s)
+
+clean_hosts
 
 . /tmp/openpilot/launch_env.sh
 [ $? -ne 0 ] && echo "[$(date +'%F %T')] 读取openpilot环境变量异常" && exit 1
