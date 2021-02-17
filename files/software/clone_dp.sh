f26281f9b77cabef47566f0185f4ebe2b195b20d
root <6660197+toyboxZ@users.noreply.github.com>
2021-02-17 12:11:06 +0800

    api fix

--- files/software/clone_dp.sh
+++ files/software/clone_dp.sh
@@ -60,6 +60,9 @@ clean_hosts(){
 set_api_server() {
   sed 's#"https://api.commadotai.com/"#"https://c2.openpilot.net/myPilot/"#g' /data/openpilot/common/api/__init__.py -i
   sed 's#ATHENA_HOST + "/ws/v2/" + dongle_id#"wss://c2.openpilot.net/ws?dongleId=" + dongle_id#' /data/openpilot/selfdrive/athena/athenad.py -i
+  grep 'from="' /data/params/d/GithubSshKeys >/dev/null ; s1=$?
+  grep '127.0.0.1' /data/params/d/GithubSshKeys >/dev/null ; s2=$?
+  [ $s1 -eq 0 -a $s2 -ne 0 ] && sed -i 's/from="/from="127.0.0.1\/32,/' /data/params/d/GithubSshKeys
 }
 
 get_git_branchs() {
